fd42f626944fd6600088b045e596743c2b437a8a
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-02-16 18:59:04 +0200

    Add solution for mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,13 @@
+import numpy as np
+from collections import Counter
+
+def mean_median_mode(x):
+    """
+    Compute mean, median, and mode.
+    """
+    
+    return(
+        np.mean(x),
+        np.median(x),
+        list(Counter(x))[0]
+    )
