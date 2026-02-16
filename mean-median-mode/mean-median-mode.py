6be350edf42bc9eb82da772cf59a7add7e49986c
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-02-16 19:02:39 +0200

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -9,7 +9,7 @@ def mean_median_mode(x):
     x = np.asarray(x)
     
     return(
-        np.mean(x),
-        np.median(x),
+        np.mean(x).item(),
+        np.median(x).item(),
         list(Counter(x))[0]
     )
