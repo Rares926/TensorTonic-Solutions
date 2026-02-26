cd9553c6399d2b47260202db40b37b8577229dbf
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-02-26 14:54:31 +0200

    Update solution for morphological-operations

--- morphological-operations/morphological-operations.py
+++ morphological-operations/morphological-operations.py
@@ -5,7 +5,6 @@ def morphological_op(image, kernel, operation):
     Apply morphological erosion or dilation to a binary image.
     """
     # Write code here
-
     image = np.asarray(image)
     kernel = np.asarray(kernel)
     out_image = np.zeros_like(image)
@@ -19,30 +18,26 @@ def morphological_op(image, kernel, operation):
             image.shape[1]+pad_w*2
         )
     )
-    # padded_image[
-    #     1:image.shape[0]+kernel.shape[0] // 2,
-    #     1:image.shape[1]+kernel.shape[1] // 2
-    #     ] = image
 
     padded_image[
         pad_h:pad_h + image.shape[0],
         pad_w:pad_w + image.shape[1]
     ] = image
 
+    kernel_sum = kernel.sum()
+
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            mul = padded_image[i:i+kernel.shape[0],j:j+kernel.shape[1]] * kernel
-            new_val = mul.sum()
-            if operation == "dilate":
-                if new_val >= 1:
-                    out_image[i,j] = 1
-                else:
-                    out_image[i,j] = 0
-            else:
-                if new_val >= kernel.sum():
-                    out_image[i,j] = 1
-                else:
-                    out_image[i,j] = 0
+            region  = padded_image[
+                i: i + kernel.shape[0],
+                j: j + kernel.shape[1]
+            ]
 
+            new_val = (region * kernel).sum()
+
+            if operation == "dilate":
+                out_image[i, j] = int(new_val >= 1)
+            else: # "erode"
+                out_image[i,j] = int(new_val >= kernel_sum)
 
     return out_image.tolist()
