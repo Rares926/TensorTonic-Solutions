8662a877332ba1e3230bebb4e3b17cbfe9f16542
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-03-03 18:35:34 +0200

    Update solution for histogram-equalization

--- histogram-equalization/histogram-equalization.py
+++ histogram-equalization/histogram-equalization.py
@@ -5,40 +5,41 @@ def histogram_equalize(image):
     Apply histogram equalization to enhance image contrast.
     """
     image = np.asarray(image)
-    max_pix = image.max() + 1
+    image_out = np.zeros_like(image)
 
-    freq_hist = np.zeros(max_pix) # frequency histogram
+    freq_hist = np.zeros(256)
 
+    # compute histogram
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             freq_hist[image[i,j]] += 1
 
-    cdf = np.zeros(max_pix)
-    cdf[0] = freq_hist[0]
-    for i in range(1,len(cdf)):
-        cdf[i] = cdf[i-1] + freq_hist[i]
+    # compute cumulative distribution function
+    # my code
+    # cdf = np.zeros(256)
+    # cdf[0] = freq_hist[0]
+    # for i in range(1,len(cdf)):
+    #     cdf[i] = cdf[i-1] + freq_hist[i]
 
-    image_out = np.zeros_like(image)
+    cdf = np.cumsum(freq_hist)
 
-    cdf_min = None
-    for cdf_vl in cdf:
-        if cdf_vl > 0 and cdf_min==None:
-            cdf_min = cdf_vl
+    # find first non zero value for the cdf
+    # cdf_min = None
+    # for cdf_vl in cdf:
+    #     if cdf_vl > 0 and cdf_min==None:
+    #         cdf_min = cdf_vl
 
-    if cdf_min==None:
-        cdf_min = 0
+    # cdf_min = 0 if cdf_min is None else cdf_min
+
+    positive = cdf[cdf > 0]
+    cdf_min = positive[0] if positive.size > 0 else 0
 
     for i in range(image_out.shape[0]):
         for j in range(image_out.shape[1]):
 
-            upper = cdf[image[i,j]]-cdf_min
-            lower = cdf.max()-cdf_min
+            upper = cdf[image[i, j]] - cdf_min
+            lower = cdf.max() - cdf_min + 1e-9
 
-            if lower == 0.0:
-                image_out[i,j] = 0.0
-                continue
-
-            image_out[i,j] = round((upper /lower) * 255)
-
+            image_out[i,j] = round((upper / lower) * 255)
 
     return image_out.tolist()
