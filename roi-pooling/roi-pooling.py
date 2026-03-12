4829bfb59686499200b13d12b5830141cad6d6c1
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-03-12 16:28:53 +0200

    Update solution for roi-pooling

--- roi-pooling/roi-pooling.py
+++ roi-pooling/roi-pooling.py
@@ -9,27 +9,32 @@ def roi_pool(feature_map, rois, output_size):
     outs = []
 
     for roi in rois:
-        roi_W = abs(roi[0] - roi[2])
-        roi_H = abs(roi[1] - roi[3])
+
+        roi_tl_x, roi_tl_y, roi_br_x, roi_br_y = roi
+
+        roi_W = abs(roi_tl_x - roi_br_x)
+        roi_H = abs(roi_tl_y - roi_br_y)
 
         out_array = np.zeros((output_size, output_size))
 
         for i in range(output_size):
             for j in range(output_size):
 
-                h_start = roi[1] + math.floor(i * roi_H / output_size)
-                h_end = roi[1] + math.floor((i+1) * roi_H  / output_size)
+                h_start = roi_tl_y + math.floor(i * roi_H / output_size)
+                h_end = roi_tl_y + math.floor((i+1) * roi_H  / output_size)
 
-                w_start = roi[0] + math.floor(j * roi_W / output_size)
-                w_end = roi[0] + math.floor((j+1) * roi_W / output_size)
+                w_start = roi_tl_x + math.floor(j * roi_W / output_size)
+                w_end = roi_tl_x + math.floor((j+1) * roi_W / output_size)
 
                 h_end = h_end if h_end != h_start else h_start + 1
                 w_end = w_end if w_end != w_start else w_start + 1
 
-                out_array[i,j] = feature_map[
+                region = feature_map[
                     h_start : h_end,
                     w_start : w_end
-                ].max() 
+                ]
+
+                out_array[i,j] = region.max()
 
         outs.append(out_array.tolist())
 
