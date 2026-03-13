687e883221fc27c680297ea148f74bcbc413ed0e
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-03-13 15:01:32 +0200

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -5,13 +5,7 @@ def vector_norm_3d(v):
     Compute the Euclidean norm of 3D vector(s).
     """
     # Your code here
-
     v = np.asarray(v)
+    axis = 0 if v.ndim==1 else 1
 
-    if len(v.shape) == 1:
-        return np.sqrt(np.sum([el**2 for el in v]))
-    else:
-        res = np.zeros(v.shape[0])
-        for idx, vect in enumerate(v):
-            res[idx] = np.sqrt(np.sum([el**2 for el in vect])).item()
-        return res
+    return np.sqrt(np.sum(v**2, axis=axis))
