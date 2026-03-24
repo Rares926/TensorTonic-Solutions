26478bad15274a4eb8e8718f3a1b567f58727931
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-03-24 17:14:18 +0200

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -11,11 +11,6 @@ def cross_entropy_loss(y_true, y_pred):
     if y_true.shape[0] != y_pred.shape[0]:
         raise Exception("y_true and y_pred must have matching first dimension N")
 
-    loss = 0.0
+    true_preds_probs = y_pred[np.arange(len(y_true)), y_true]
 
-    for idx, pred in enumerate(y_pred):
-        loss += -np.log(pred[y_true[idx]])
-
-    normalized_loss = loss/len(y_true)
-
-    return normalized_loss
+    return np.mean(-np.log(true_preds_probs))
