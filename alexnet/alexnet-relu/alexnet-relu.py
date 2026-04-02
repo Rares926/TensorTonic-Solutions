955738457a183a2b8ae5dd2548492bcefa9353e9
Rares926 <62502494+Rares926@users.noreply.github.com>
2026-04-02 17:32:55 +0300

    Add solution for alexnet-relu

---
+++ alexnet/alexnet-relu/alexnet-relu.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def relu(x: np.ndarray) -> np.ndarray:
+    """ReLU activation: f(x) = max(0, x)"""
+    # YOUR CODE HERE
+    return np.where(
+        x>=0,
+        x,
+        0
+    )
